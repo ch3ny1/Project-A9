6317f51cc1cbafc5e310c2e05047fecb468bc3c0
Lawrence Wang <49330756+ch3ny1@users.noreply.github.com>
2020-06-21 17:41:04 -0500

    Add files via upload

--- MP_Farming.py
+++ MP_Farming.py
@@ -17,6 +17,7 @@ from matplotlib import pyplot as plt
 #indicator_entering = cv.imread('indicator.ppm')
 #checker_exit = cv.imread('checker.ppm')
 redflag = cv.imread('redFlag.ppm')
+redflag = cv.cvtColor(redflag, cv.COLOR_BGR2GRAY)
 bckgrd = cv.imread('non.ppm')
 missed_frame = cv.imread('missed.ppm')
 
@@ -33,18 +34,18 @@ async def farm(controller_state: ControllerState):
     await button_push(controller_state, 'zr', sec=0.1)
     await asyncio.sleep(0.1)
     await button_push(controller_state, 'a', sec=0.1)
-    await asyncio.sleep(0.1)
+    #await asyncio.sleep(0.1)
     #await button_push(controller_state, 'zr', sec=0.1)
     await asyncio.sleep(0.4)
     #await button_push(controller_state, 'right', sec=0.034)
     #await asyncio.sleep(0.034)
     #await asyncio.sleep(0.4)
-    await button_push(controller_state, 'down', sec=0.034)
+    await button_push(controller_state, 'down')
     await asyncio.sleep(0.034)
     await button_push(controller_state, 'left')
     #await asyncio.sleep(0.034)
     await button_push(controller_state, 'right')
-    await asyncio.sleep(0.2)
+    await asyncio.sleep(0.1)
 
 
 
@@ -53,7 +54,7 @@ async def running(controller_state: ControllerState):
     await asyncio.sleep(0.4)
     await button_push(controller_state, 'plus',sec=0.1)
     await asyncio.sleep(1)
-    await button_push(controller_state, 'down', sec=1.3)
+    await button_push(controller_state, 'down', sec=1.5)
     await asyncio.sleep(0.5)
     await button_push(controller_state, 'a',sec=0.1)
     await asyncio.sleep(5)
@@ -106,7 +107,7 @@ async def time2run(loaded=False, waiting2quit=False):
     return False
 """
 
-def time2run(waiting2quit=False, loaded=False):
+async def time2run(controller_state: ControllerState, waiting2quit=False, loaded=False):
     global count_played
     global count_quitted
 
@@ -118,36 +119,66 @@ def time2run(waiting2quit=False, loaded=False):
             print("Skipped a frame")
             continue
 
-        Pos_1 = frame[220:228, 280:288, 0:3]
+        #cv.imshow('frame', frame)
+        Pos_1 = cv.cvtColor(frame[220:228, 280:288, 0:3], cv.COLOR_BGR2GRAY)
+
         Pos_2 = frame[730:736, 992:998, 0:3]
 
-        loading = not cv.absdiff(Pos_1, redflag).any()
+        print('Diff:' + str(cv.absdiff(Pos_1, redflag).sum()))
+        loading = (not cv.absdiff(Pos_1, redflag).any()) or (cv.absdiff(Pos_1, redflag).sum()<100) or (cv.absdiff(Pos_1, redflag).sum()>10600)
 
         if loading:
-            if waiting2quit:
+            if not loaded:
+                print('loading...')
+                loaded = True
                 continue
 
-            elif cv.absdiff(Pos_2, bckgrd).any():
+            elif waiting2quit:
+                continue
+
+            if cv.absdiff(Pos_2, bckgrd).any():
                 waiting2quit = True
                 print('Too many people. Prepare for withdrawl.')
                 count_quitted += 1
+                print('Number of withdrawls: ' + str(count_quitted))
                 continue
             else:
+                print('Nice round. Prepare for 9500 credits!')
+                count_played += 1
+                print('Number of rounds played: ' + str(count_played))
                 loaded = True
+                waiting2quit=False
                 continue
-        else:
-            break
 
+                #loaded = False
+                #return False
+        elif waiting2quit:
+            print('Outta here')
+            await running(controller_state)
+            print('Run away and start again')
+            #break
+            loaded = False
+            waiting2quit = False
+            return False
+        else:
+            loaded = False
+            waiting2quit = False
+            return False
+"""
     if waiting2quit:
-        #waiting2quit = False
+        waiting2quit = False
         return True
     elif loaded:
         print('Nice round. Prepare for 9500 credits!')
         count_played += 1
+        print('Number of rounds played: ' + str(count_played))
+        loaded = False
         return False
     else:
         return False
 
+    return False
+"""
 
 
 async def farmInt(controller_state: ControllerState):
@@ -163,25 +194,18 @@ async def farmInt(controller_state: ControllerState):
     )
 
     while not user_input.done():
+        if user_input.done():
+            farming = False
+
+
         if not farming:
             cap.release()
             cv.destroyAllWindows()
             break
 
         #while not await time2run():
-        elif not time2run(False, False):
+        elif not await time2run(controller_state, False, False):
             await farm(controller_state)
-
-            if user_input.done():
-                farming = False
-                break
-
-        else:
-            print('Outta here')
-            await running(controller_state)
-            print('Run away and start again')
-
-
 
     print('Played: ' + str(count_played))
     print('Quitted: ' + str(count_quitted))
@@ -189,3 +213,11 @@ async def farmInt(controller_state: ControllerState):
     #cap.release()
     #cv.destroyAllWindows()
     await user_input
+
+"""
+        else:
+            print('Outta here')
+            await running(controller_state)
+            print('Run away and start again')
+
+"""
